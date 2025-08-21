db03e699003c99dd3a27f01b204830a477269933
lorna88 <lorna_88@mail.ru>
2025-08-21 11:49:43 +0300

    Added view and template for task card

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
 
-# Create your views here.
+from .models import Task
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'tasks/task-details.html'

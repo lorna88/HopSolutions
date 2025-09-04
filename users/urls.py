8c8f3f1962de4a3da48964b15962153866f0d24e
lorna88 <lorna_88@mail.ru>
2025-09-04 11:35:03 +0300

    Added logout logic on backend, redirect after login/logout

--- users/urls.py
+++ users/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('register/', views.UserCreateView.as_view(), name='register'),
     path('login/', LoginView.as_view(
         template_name = 'users/login.html',
+        next_page=reverse_lazy('tasks:home'),
     ), name='login'),
-    path('home/', views.HomeView.as_view(), name='home'),
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy('users:login')), name='logout'),
     path('account/', views.AccountView.as_view(), name='account'),
 ]

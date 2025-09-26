66d3e6216f226d50b4a691ec72552f4372b5f798
lorna88 <lorna_88@mail.ru>
2025-09-26 20:12:10 +0300

    Customizing model display on admin panel - created new templates: base templates and templates for list objects

--- tasks/admin.py
+++ tasks/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import Category, Task
 
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'date', 'user', 'is_completed')
+    list_per_page = 10
+
 admin.site.register(Category)
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)

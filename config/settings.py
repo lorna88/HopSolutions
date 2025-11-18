287661dc33cc48a0cc1bdd3e525e31e04e1589ba
lorna88 <lorna_88@mail.ru>
2025-11-18 15:42:40 +0300

    Develop settings

--- config/settings.py
+++ config/settings.py
@@ -70,14 +70,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': os.getenv('POSTGRES_DB'),
-    #     'USER': os.getenv('POSTGRES_USER'),
-    #     'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-    #     'HOST': os.getenv('POSTGRES_HOST', 'db'),
-    #     'PORT': os.getenv('POSTGRES_PORT', '5432' ),
-    # }
 }
 
 
